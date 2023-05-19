3358a40b603e5fa75aeeae30d970c9e5531b72a9
handsome <handsome@gmail.com>
2023-05-19 16:23:47 +0800

    refactored UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg'
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
 import './App.css'
 
+const useStyles = makeStyles((theme) => ({
+  // Remove the inline styles
+}))
+
 function App() {
+  const classes = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload 111222大大的
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={`${classes.menuButton} menuButton`}
+            color="inherit"
+            aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={`${classes.title} title`}>
+            My Awesome App
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <div className="content">
+        <Typography variant="h4" gutterBottom>
+          Welcome to My Awesome App!
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          This is a more complex and visually appealing interface created using React and
+          Material-UI.
+        </Typography>
+        <Button variant="contained" color="primary">
+          Get Started
+        </Button>
+      </div>
     </div>
   )
 }
